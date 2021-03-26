9d031f5db881b1c1ca05abd2be408030fabca452
panzh <panzheng1999@outlook.com>
2021-03-26 20:56:41 +0800

    This is a pz

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Hello PZ");
         primaryStage.setScene(new Scene(root, 200, 275));
         primaryStage.show();
     }
