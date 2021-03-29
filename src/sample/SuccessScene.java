660df6584c74c1e3802ff0df099915d573cc4b49
panzh <panzheng1999@outlook.com>
2021-03-29 22:25:45 +0800

    Update with prototype

---
+++ src/sample/SuccessScene.java
@@ -0,0 +1,16 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+public class SuccessScene {
+    /**
+     * this function is called OkButtonClicked, once user is informed of their changing action successfully, he click it to next steps.
+     * @param actionEvent
+     */
+    public void OkButtonClicked(ActionEvent actionEvent) {
+        Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        window.close();
+    }
+}
