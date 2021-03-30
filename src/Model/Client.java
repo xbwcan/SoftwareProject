29465f7e086b756437a9372e626e99d166b13a4d
xbwcan <724170134@qq.com>
2021-03-30 14:47:31 +0800

    3.30 zz

---
+++ src/Model/Client.java
@@ -0,0 +1,26 @@
+package Model;
+
+public class Client {
+    public String Name, E_Mail, Phone_Number, Pasword;
+
+}
+/*
+* 1. name     String
+	2. Phone_number String
+	3. E-mail String
+	4. Weight int
+	5. height int
+	6. BMI    int
+	7. Fatty_Lipase  int
+	8. Age	int
+	9. Rank int //用户等级
+	10. Rank_Start_Time long long 单位是秒
+	11. Rank_End_Time long long 同上
+	12. My_Classes[]  Class
+	13. My_Lives[]	Live
+	14. State String
+	15. Generic_Plans Generic_Plan
+	function:
+		.Set_{name}()
+		.Get_{name}()
+		.Get_discount()
